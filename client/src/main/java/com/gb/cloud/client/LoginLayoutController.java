cfa9cce9fe8fdb4305b18110fc60b459f7782175
Kir <rapidsilver@example.com>
2018-09-26 13:34:12 +0300

    Готовы черновики формы регистрации и авторизации

--- client/src/main/java/com/gb/cloud/client/LoginLayoutController.java
+++ client/src/main/java/com/gb/cloud/client/LoginLayoutController.java
@@ -3,12 +3,16 @@ package com.gb.cloud.client;
 import com.gb.cloud.common.MyFile;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-
+import java.io.IOException;
 
 
 public class LoginLayoutController {
@@ -29,5 +33,21 @@ Button regButton;
     public void autorize(ActionEvent actionEvent) {
         System.out.println(loginField.getText());
         System.out.println(passField.getText());
+    }
+
+
+    public void goToRegpage(ActionEvent actionEvent) {
+        regButton.getScene().getWindow().hide();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/regLayout.fxml"));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
     }
 }
